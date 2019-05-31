c69aa544bc173260896706bf5c7c4723a1d5093e
Victor Almeida França <fr4ncacc@gmail.com>
2019-05-31 09:51:25 -0300

    delete post

--- src/components/PostCard.vue
+++ src/components/PostCard.vue
@@ -4,24 +4,37 @@
       <div class="card-header text-center">{{ this.post.titulo }}</div>
       <div class="card-body">{{ this.post.corpo }}</div>
       <div class="card-footer">
-        <div class="text-muted text-right small">
+        <div class="text-muted float-left small">
           Escrito por:
           <strong>{{ this.post.user.name }}</strong>
           em
           <strong>{{ this.post.created_at }}</strong>
         </div>
+        <button
+          @click="deletePost(post.id)"
+          v-if="this.post.user.id == user.id"
+          class="float-right btn btn-sm btn-danger"
+        >x</button>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import { mapActions, mapGetters } from "vuex";
+
 export default {
   name: "PostCard",
   props: {
     post: {
       type: Object
     }
+  },
+  computed: {
+    ...mapGetters(["user"])
+  },
+  methods: {
+    ...mapActions(["deletePost"])
   }
 };
 </script>
--- src/store/modules/posts.js
+++ src/store/modules/posts.js
@@ -1,46 +1,59 @@
-import axios from '../../services/api'
-import moment from 'moment'
+/* eslint-disable */
+
+import axios from "../../services/api";
+import moment from "moment";
 
 const state = {
   posts: []
-}
+};
 
 const getters = {
   allPosts: state => state.posts
-}
+};
 
 const actions = {
   async getAllPosts({ commit }) {
-    const res = await axios.get('/posts')
+    const res = await axios.get("/posts");
     res.data.map(
       post =>
         (post.created_at = moment(post.created_at)
           .utc()
-          .format('DD/MM/YYYY HH:mm:ss'))
-    )
-    commit('setPosts', res.data)
+          .format("DD/MM/YYYY HH:mm:ss"))
+    );
+    commit("setPosts", res.data);
   },
   async addPost({ commit }, post) {
     try {
-      const { data: newPost } = await axios.post('/posts', post)
+      const { data: newPost } = await axios.post("/posts", post);
       newPost.created_at = moment(newPost.created_at)
         .utc()
-        .format('DD/MM/YYYY HH:mm:ss')
-      commit('addNewPost', newPost)
+        .format("DD/MM/YYYY HH:mm:ss");
+      commit("addNewPost", newPost);
     } catch (e) {
-      console.log(e.response.data)
+      console.log(e.response.data);
+    }
+  },
+  async deletePost({ commit }, _id) {
+    try {
+      await axios.delete(`/post/${_id}`);
+
+      commit("mutDeletePost", _id);
+    } catch (e) {
+      console.log(e.response.data);
     }
   }
-}
+};
 
 const mutations = {
   setPosts: (state, posts) => (state.posts = posts),
-  addNewPost: (state, post) => state.posts.unshift(post)
-}
+  addNewPost: (state, post) => state.posts.unshift(post),
+  mutDeletePost: (state, _id) =>
+    (state.posts = state.posts.filter(post => post.id !== _id))
+};
 
 export default {
   state,
   getters,
   actions,
   mutations
-}
+};
